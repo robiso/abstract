6c09d333e7f4681d10335a99fca8e43b244524db
Robert Isoski <r@robiso.com>
2020-02-10 20:15:56 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?=wCMS::get('config','siteTitle')?> - <?=wCMS::page('title')?></title>
-	<meta name="description" content="<?=wCMS::page('description')?>">
-	<meta name="keywords" content="<?=wCMS::page('keywords')?>">
+	<title><?=$Wcms->get('config','siteTitle')?> - <?=$Wcms->page('title')?></title>
+	<meta name="description" content="<?=$Wcms->page('description')?>">
+	<meta name="keywords" content="<?=$Wcms->page('keywords')?>">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?=wCMS::asset('css/style.css')?>">
-	<?=wCMS::css()?>
+	<?=$Wcms->css()?>
+	<link rel="stylesheet" href="<?=$Wcms->asset('css/style.css')?>">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
 
    <script type="text/javascript">
@@ -31,20 +33,20 @@
    </script>
 </head>
 <body>
-	<?=wCMS::settings()?>
-	<?=wCMS::alerts()?>
+	<?=$Wcms->settings()?>
+	<?=$Wcms->alerts()?>
 	<nav class="navbar navbar-default">
 		<div class="container">
 			<div class="col-sm-5 text-center">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navMobile">&#9776;</button>
-					<a href="./"><h1><?=wCMS::get('config','siteTitle')?></h1></a>
+					<a href="./"><h1><?=$Wcms->get('config','siteTitle')?></h1></a>
 				</div>
 			</div>
 			<div class="col-sm-7 text-center">
 				<div class="collapse navbar-collapse" id="navMobile">
 					<ul class="nav navbar-nav navbar-right">
-						<?=wCMS::menu()?>
+						<?=$Wcms->menu()?>
 					</ul>
 				</div>
 			</div>
@@ -54,26 +56,26 @@
 	<div class="container">
 		<div class="col-xs-12 col-sm-8">
 			<div class="whiteBackground grayFont padding20 rounded5">
-				<?=wCMS::page('content')?>
+				<?=$Wcms->page('content')?>
 			</div>
 		</div>
 		<div class="col-xs-12 col-sm-4">
 			<div class="visible-xs spacer20"></div>
 			<div class="blueBackground padding20 rounded5">
-				<?=wCMS::block('subside')?>
+				<?=$Wcms->block('subside')?>
 			</div>
 		</div>
 	</div>
 
 	<footer class="container-fluid">
 		<div class="padding20 text-right">
-			<?=wCMS::footer()?>
+			<?=$Wcms->footer()?>
 		</div>
 	</footer>
 
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.autosize/3.0.17/autosize.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<?=wCMS::js()?>
+	<?=$Wcms->js()?>
 </body>
 </html>
